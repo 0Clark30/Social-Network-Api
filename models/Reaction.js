168441e3cf730acd68f3f29cd7eb9f4508b91346
0Clark30 <austinlaw.clark@outlook.com>
2024-02-22 10:01:33 -0600

    added to models (reaction) (in progress)

--- models/Reaction.js
+++ models/Reaction.js
@@ -2,6 +2,32 @@ const {Schema, model} = require("mongoose");
 
 const reactionSchema = new Schema (
 {
-    reactionId: { }
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        Required: true,
+        // set something for 280 character max
+    },
+    username: {
+        type: String,
+        Required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+
+    }
 }
-)
+{
+    toJSON: {
+        virtuals: true,
+    }
+}
+);
+
+//  need to set a getter method to format the timestamp on query
+
+
